80d1c6d88ae764d0559eaeb05044ed7cf2dbbad0
Sium13 <khalidmahmudsium542543@gmail.com>
2021-10-31 18:16:27 +0600

    notun

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wnpsb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-// console.log(uri);
+console.log(uri);
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
